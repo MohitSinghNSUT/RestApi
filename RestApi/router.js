adf567025cf3bb6e364071bc201693ae2348eb75
MohitSinghNSUT <118382421+MohitSinghNSUT@users.noreply.github.com>
2024-12-04 14:10:02 +0530

    Delete RestApi directory

--- RestApi/router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const { getproducts } = require('./controller');
-console.log(getproducts);
-router.route('/new').get(getproducts);
-module.exports = router;
